d9e012f4a10b112f3146d0ebdd07374dc72dd937
UQJRlex <xuzhuyin527@gmail.com>
2025-02-17 15:33:19 +0800

    KernelSU: core_hook.c: add lsposed to umount

--- kernel/core_hook.c
+++ kernel/core_hook.c
@@ -885,6 +885,9 @@ void susfs_try_umount_all(uid_t uid) {
 	/* For both Legacy KSU and Magic Mount KSU */
 	ksu_try_umount("/debug_ramdisk", true, MNT_DETACH, uid);
 	ksu_try_umount("/system/etc/hosts", false, MNT_DETACH, uid);
+	// lsposed
+	ksu_try_umount("/apex/com.android.art/bin/dex2oat64", false, MNT_DETACH, uid);
+	ksu_try_umount("/apex/com.android.art/bin/dex2oat32", false, MNT_DETACH, uid);
 }
 #endif
 
@@ -983,6 +986,10 @@ out_ksu_try_umount:
 
 	// try umount /system/etc/hosts (hosts module)
 	ksu_try_umount("/system/etc/hosts", false, MNT_DETACH);
+
+	// lsposed
+	ksu_try_umount("/apex/com.android.art/bin/dex2oat64", false, MNT_DETACH, uid);
+	ksu_try_umount("/apex/com.android.art/bin/dex2oat32", false, MNT_DETACH, uid);
 #endif
 
 	return 0;
